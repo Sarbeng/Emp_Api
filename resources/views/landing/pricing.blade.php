8e11b8d7d9a5dc2663373f04c8aa473f088c9449
Sarbeng <sarbengbaafi@gmail.com>
2023-07-21 19:06:38 +0000

    pricing

--- resources/views/landing/pricing.blade.php
+++ resources/views/landing/pricing.blade.php
@@ -1,3 +1,92 @@
-<section class="px-[120px]">
-    
+<section id="services" class="px-[120px] py-8">
+    <h2 class="text-sub-title text-center">Plans and Pricing</h2>
+    <section class="mt-10 flex gap-14">
+        {{-- card 1 --}}
+        <section
+            class="p-12 bg-slate-100 rounded-lg  text-center flex flex-col gap-16 justify-center items-center w-1/3">
+            <div>
+                <h2 class="text-sub-title text-center">Basic</h2>
+                <h5>Free</h5>
+            </div>
+            <ul class="flex flex-col gap-6">
+                <li class="flex items-center gap-3">
+                    <img src="icons/check.svg" alt="" class="w-6" />
+                    <p>Lorem Ipsum</p>
+                </li>
+                <li class="flex items-center gap-3">
+                    <img src="icons/check.svg" alt="" class="w-6" />
+                    <p>Lorem Ipsum</p>
+                </li>
+                <li class="flex items-center gap-3">
+                    <img src="icons/check.svg" alt="" class="w-6" />
+                    <p>Lorem Ipsum</p>
+                </li>
+                <li class="flex items-center gap-3">
+                    <img src="icons/check.svg" alt="" class="w-6" />
+                    <p>Lorem Ipsum</p>
+                </li>
+            </ul>
+            <button class="bg-accent text-white px-8 py-3 rounded-lg text-lead-text">
+                Get Started
+            </button>
+        </section>
+        {{-- card 2 --}}
+        <section
+            class="p-12 bg-accent text-white rounded-lg  text-center flex flex-col gap-16 justify-center items-center w-1/3">
+            <div>
+                <h2 class="text-sub-title text-center">Pro</h2>
+                <h5>GHS 20.00</h5>
+            </div>
+            <ul class="flex flex-col gap-6">
+                <li class="flex items-center gap-3">
+                    <img src="icons/check_white.svg" alt="" class="w-6" />
+                    <p>Lorem Ipsum</p>
+                </li>
+                <li class="flex items-center gap-3">
+                    <img src="icons/check_white.svg" alt="" class="w-6" />
+                    <p>Lorem Ipsum</p>
+                </li>
+                <li class="flex items-center gap-3">
+                    <img src="icons/check_white.svg" alt="" class="w-6" />
+                    <p>Lorem Ipsum</p>
+                </li>
+                <li class="flex items-center gap-3">
+                    <img src="icons/check_white.svg" alt="" class="w-6" />
+                    <p>Lorem Ipsum</p>
+                </li>
+            </ul>
+            <button class="bg-secondary text-text px-8 py-3 rounded-lg text-lead-text">
+                Get Started
+            </button>
+        </section>
+        {{-- card 3 --}}
+        <section
+            class="p-12 bg-slate-100 rounded-lg  text-center flex flex-col gap-16 justify-center items-center w-1/3">
+            <div>
+                <h2 class="text-sub-title text-center">Enterprise</h2>
+                <h5>GHS 20.00</h5>
+            </div>
+            <ul class="flex flex-col gap-6">
+                <li class="flex items-center gap-3">
+                    <img src="icons/check.svg" alt="" class="w-6" />
+                    <p>Lorem Ipsum</p>
+                </li>
+                <li class="flex items-center gap-3">
+                    <img src="icons/check.svg" alt="" class="w-6" />
+                    <p>Lorem Ipsum</p>
+                </li>
+                <li class="flex items-center gap-3">
+                    <img src="icons/check.svg" alt="" class="w-6" />
+                    <p>Lorem Ipsum</p>
+                </li>
+                <li class="flex items-center gap-3">
+                    <img src="icons/check.svg" alt="" class="w-6" />
+                    <p>Lorem Ipsum</p>
+                </li>
+            </ul>
+            <button class="bg-accent text-white px-8 py-3 rounded-lg text-lead-text">
+                Get Started
+            </button>
+        </section>
+    </section>
 </section>
